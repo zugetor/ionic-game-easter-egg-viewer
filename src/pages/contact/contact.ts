c0c2ff141a488327633436c2e850099017dddfa3
ZuGeTor <14806377+zugetor@users.noreply.github.com>
2019-04-26 00:42:56 +0700

    Add Map Function

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +7,44 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private platform: Platform) {
 
   }
+  
+  openUrl(app: string) {
+	  /*
+        switch (app) {
+            case 'facebook':
+                if (this.platform.is('ios')) {
+					window.open('fb://profile/726019957484156', '_self');
+				} else if (this.platform.is('android')) {
+					window.open('fb://facewebmodal/f?href=https://www.facebook.com/zugetor', '_self');
+				} else {
+					window.open('https://www.facebook.com/zugetor', '_self');
+				}
+                break;
+            case 'twitter':
+                if (this.platform.is('ios')) {
+					window.open('twitter:///user?screen_name=zugetor', '_self');
+				} else if (this.platform.is('android')) {
+					window.open('twitter://user?screen_name=zugetor', '_self');
+				} else {
+					window.open('https://www.twitter.com/zugetor', '_self');
+				}
+                break;
+			case 'youtube':
+                if (this.platform.is('ios')) {
+					window.open('youtube://www.youtube.com/c/ZuGeToriT', '_self');
+				} else {
+					window.open('https://www.youtube.com/c/ZuGeToriT', '_self');
+				}
+                break;
+			case 'github':
+				window.open('https://github.com/sunsunza2009', '_self');
+                break;
+            default:
+                break;
+        }*/
+    }
 
 }
--- src/pages/gamedetail/gamedetail.ts
+++ src/pages/gamedetail/gamedetail.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Map, tileLayer } from 'leaflet';
 /**
  * Generated class for the GamedetailPage page.
  *
@@ -14,11 +14,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'gamedetail.html',
 })
 export class GamedetailPage {
-
+  map: Map;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GamedetailPage');
+	
   }
+  ionViewDidEnter() {
+    this.loadmap();
+  }
+  ionViewWillLeave() {
+    this.map.remove();
+  }
+ 
+  loadmap() {
+    this.map = new Map('map').fitWorld();
+
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(this.map);
+
+	}
+
 }
