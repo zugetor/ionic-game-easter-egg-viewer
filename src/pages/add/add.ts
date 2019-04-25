70d858f222c8357f7ad7f123963ffcb996f293c6
Chatcha Tanyeenguan <59161153@go.buu.ac.th>
2019-04-26 01:21:09 +0700

    commit addPage

--- src/pages/add/add.ts
+++ src/pages/add/add.ts
@@ -16,10 +16,59 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class AddPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.initializeItems();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddPage');
   }
+  items;
 
+
+  initializeItems() {
+    this.items = [
+      'XCOM 2',
+      'Dishonored 2',
+      'Total War: Warhammer 2',
+      'League of Legends',
+      'Dark Souls 3',
+      'Stardew Valley',
+      'God of war 3',
+      'Fallout 4',
+      'Grand Theft Auto 5',
+      'red dead redemption 2',
+      'Subnautica',
+      'Assassin\'s creed Origins',
+      'Diablo 3',
+      'Assassin\'s Creed Odyssey',
+      'Devil May Cry 5',
+      'Metro Exodus',
+      'Counter-Strike: Global Offensive',
+      'Undertale',
+      'The Elder Scrolls 5: Skyrim',
+      'Mafia 2',
+      'Bioshock Infinite',
+      'Overwatch',
+      'Marvel\'s Spider-Man',
+      'Minecraft',
+      'Uncharted 4',
+      'The Witcher 3: Wild Hunt',
+      'Borderlands'
+    ];
+  }
+
+  getItems(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
 }
