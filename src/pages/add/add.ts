158bbc460c8093dff0f863fa4a86bb29a164d7db
ZuGeTor <14806377+zugetor@users.noreply.github.com>
2019-04-27 18:32:00 +0700

    Edit Add Page

--- src/pages/add/add.ts
+++ src/pages/add/add.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage,NavController,NavParams} from 'ionic-angular';
+import {FormBuilder,FormArray,FormGroup,Validators} from '@angular/forms';
 
 /**
  * Generated class for the AddPage page.
@@ -10,76 +11,101 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-add',
-  templateUrl: 'add.html',
+	selector: 'page-add',
+	templateUrl: 'add.html',
 })
 export class AddPage {
+	public form_egg: FormGroup;
+	constructor(public navCtrl: NavController, public navParams: NavParams, private _FB: FormBuilder) {
+		this.initializeItems();
+		 this.form_egg = this._FB.group({			 
+			 egg     : this._FB.array([
+				this.initTechnologyFields()
+			 ])
+		  });
+	}
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.initializeItems();
-  }
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad AddPage');
+		this.selectGame = "Select Game!!";
+		this.description = "please select game to add easter eggs";
+	}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddPage');
-  this.selectGame = "Select Game!!";
-  this.description = "please select game to add easter eggs";
-  }
+	items;
+	selectGame;
+	description;
+	game;
 
-  items;
-  selectGame;
-  description;
-  game;
-  
-  chooseGame(game:string){
-    this.selectGame=game;
-    this.description = "";
-  }
+	chooseGame(game: string) {
+		this.selectGame = game;
+		this.description = "";
+	}
 
-  initializeItems() {
-    this.items = [
-      'XCOM 2',
-      'Dishonored 2',
-      'Total War: Warhammer 2',
-      'League of Legends',
-      'Dark Souls 3',
-      'Stardew Valley',
-      'God of war 3',
-      'Fallout 4',
-      'Grand Theft Auto 5',
-      'red dead redemption 2',
-      'Subnautica',
-      'Assassin\'s creed Origins',
-      'Diablo 3',
-      'Assassin\'s Creed Odyssey',
-      'Devil May Cry 5',
-      'Metro Exodus',
-      'Counter-Strike: Global Offensive',
-      'Undertale',
-      'The Elder Scrolls 5: Skyrim',
-      'Mafia 2',
-      'Bioshock Infinite',
-      'Overwatch',
-      'Marvel\'s Spider-Man',
-      'Minecraft',
-      'Uncharted 4',
-      'The Witcher 3: Wild Hunt',
-      'Borderlands'
-    ];
-  }
+	initializeItems() {
+		this.items = [
+			'XCOM 2',
+			'Dishonored 2',
+			'Total War: Warhammer 2',
+			'League of Legends',
+			'Dark Souls 3',
+			'Stardew Valley',
+			'God of war 3',
+			'Fallout 4',
+			'Grand Theft Auto 5',
+			'red dead redemption 2',
+			'Subnautica',
+			'Assassin\'s creed Origins',
+			'Diablo 3',
+			'Assassin\'s Creed Odyssey',
+			'Devil May Cry 5',
+			'Metro Exodus',
+			'Counter-Strike: Global Offensive',
+			'Undertale',
+			'The Elder Scrolls 5: Skyrim',
+			'Mafia 2',
+			'Bioshock Infinite',
+			'Overwatch',
+			'Marvel\'s Spider-Man',
+			'Minecraft',
+			'Uncharted 4',
+			'The Witcher 3: Wild Hunt',
+			'Borderlands'
+		];
+	}
 
-  getItems(ev) {
-    // Reset items back to all of the items
-    this.initializeItems();
+	getItems(ev) {
+		// Reset items back to all of the items
+		this.initializeItems();
 
-    // set val to the value of the ev target
-    var val = ev.target.value;
+		// set val to the value of the ev target
+		var val = ev.target.value;
 
-    // if the value is an empty string don't filter the items
-    if (val && val.trim() != '') {
-      
-      this.items = this.items.filter((item) => {
-        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
-      })
-    }
-  }
+		// if the value is an empty string don't filter the items
+		if (val && val.trim() != '') {
+
+			this.items = this.items.filter((item) => {
+				return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+			})
+		}
+	}
+
+	initTechnologyFields(): FormGroup {
+		return this._FB.group({
+			easter_name: ['', Validators.required],
+			easter_detail: ['', Validators.required]
+		});
+	}
+
+	addNewInputField(): void {
+		const control = < FormArray > this.form_egg.controls.egg;
+		control.push(this.initTechnologyFields());
+	}
+	removeInputField(i: number): void {
+		const control = < FormArray > this.form_egg.controls.egg;
+		control.removeAt(i);
+	}
+
+	manage(val: any): void {
+		console.dir(val);
+	}
 }
